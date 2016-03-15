dffca56da951dddb6eeaab7f062b11d21d268e52
Jim Margetts <jim.margetts@gmail.com>
2016-03-15 17:05:43 +0000

    mocha and tests

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ module.exports = function() {
   var app = express();
 
   app.get('/', function(req, res) {
-    res.send('Hello world');
+    res.send('Hello, world');
   });
 
   app.get('/user/:user', function(req, res) {
